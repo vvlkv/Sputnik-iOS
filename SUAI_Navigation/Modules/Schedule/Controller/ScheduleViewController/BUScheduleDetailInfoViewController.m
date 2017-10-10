ca40d8a0bc24638d00fdc2014a559c3f32c41b39
Joao-Victor-da-Silva-Junior <vitekliverpool95@gmail.com>
2017-10-10 18:29:22 +0300

    About app fix

---
+++ SUAI_Navigation/Modules/Schedule/Controller/ScheduleViewController/BUScheduleDetailInfoViewController.m
@@ -0,0 +1,26 @@
+//
+//  BUScheduleDatailInfoViewController.m
+//  SUAI_Navigation
+//
+//  Created by Виктор on 23.07.17.
+//  Copyright © 2017 Viktor. All rights reserved.
+//
+
+#import "BUScheduleDetailInfoViewController.h"
+
+@interface BUScheduleDetailInfoViewController ()
+
+@end
+
+@implementation BUScheduleDetailInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.output viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
--- SUAI_Navigation/Modules/Settings/MainSettings/View/About app view controller/BUAboutAppViewController.m
+++ SUAI_Navigation/Modules/Settings/MainSettings/View/About app view controller/BUAboutAppViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "BUAboutAppViewController.h"
+#import "UIColor+SUAI.h"
 
 @interface BUAboutAppViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *siteLabel;
+@property (weak, nonatomic) IBOutlet UILabel *vkLabel;
 
 @end
 
@@ -17,10 +21,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"О приложении";
+    UITapGestureRecognizer *tapOnVkGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapVkLabel:)];
+    self.vkLabel.userInteractionEnabled = YES;
+    self.vkLabel.textColor = [UIColor suaiBlueColor];
+    self.vkLabel.highlighted = YES;
+    self.vkLabel.highlightedTextColor = [UIColor suaiBlueColor];
+    [self.vkLabel addGestureRecognizer:tapOnVkGesture];
+    
+    UITapGestureRecognizer *tapOnSiteGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapSiteLabel:)];
+    self.siteLabel.userInteractionEnabled = YES;
+    self.siteLabel.textColor = [UIColor suaiBlueColor];
+    self.siteLabel.highlighted = YES;
+    self.siteLabel.highlightedTextColor = [UIColor suaiBlueColor];
+    [self.siteLabel addGestureRecognizer:tapOnSiteGesture];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+- (void)didTapSiteLabel:(UITapGestureRecognizer *)recognizer {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://sputnik.guap.ru"]];
+}
+
+- (void)didTapVkLabel:(UITapGestureRecognizer *)recognizer {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://vk.com/suainavigation"]];
+}
+
 
 @end
